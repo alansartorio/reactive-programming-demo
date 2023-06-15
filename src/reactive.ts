5e32b1981eb97ba1cda1f02b1187cf246229e3b2
aBenve <abenvenuto@itba.edu.ar>
2023-06-15 15:14:21 -0300

    😔😔😔🇾🇪

--- src/reactive.ts
+++ src/reactive.ts
@@ -1,23 +1,30 @@
- import { debounceTime, map, startWith, switchMap } from 'rxjs/operators'
-import { Observable, from, fromEvent } from 'rxjs'
-import countries from './data/countries'
+import { debounceTime, map, startWith, switchMap } from "rxjs/operators";
+import { Observable, from, fromEvent } from "rxjs";
+import countries from "./data/countries";
 
 function timeout(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function findCountriesAPI(query: string): Promise<string[]> {
   await timeout(1000);
-  return countries.filter(country => country.toLowerCase().includes(query.toLowerCase()));
-}
-
-function findCountries(query$: Observable<string>): Observable<string[] | null> {
-  return query$.pipe(
-    switchMap((query) => from(findCountriesAPI(query)).pipe(startWith(null))),
+  return countries.filter((country) =>
+    country.toLowerCase().includes(query.toLowerCase())
   );
 }
- 
+
+function findCountries(
+  query$: Observable<string>
+): Observable<string[] | null> {
+  return query$.pipe(
+    switchMap((query) => from(findCountriesAPI(query)).pipe(startWith(null)))
+  );
+}
+
 export const useCountries = (ref: HTMLInputElement, time: number) => {
-  const values = fromEvent(ref, "input").pipe(map((event) => event.target?.value), startWith(ref.value));
-  return findCountries(values.pipe(debounceTime(time)))
+  const values = fromEvent(ref, "input").pipe(
+    map((event) => (event.target as HTMLInputElement).value),
+    startWith(ref.value)
+  );
+  return findCountries(values.pipe(debounceTime(time)));
 };
